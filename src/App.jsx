5997fcbec70a62fcbd22c07c6f9d742bfcd735ea
Tomas Alonso <t.alonsoarias@itecriocuarto.org.ar>
2025-11-03 17:26:59 -0300

    creando ProteccionRutas.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./components/Home"
 import RegisterForm from "./components/RegisterForm"
 import LoginForm from "./components/LoginForm"
+import PrivateRoute from "./components/ProteccionRuntas"
 function App() {
 
   return (
     <>
     <Routes>
+
       <Route path="/" element={<Home />} />
       <Route path="/registrarse" element={<RegisterForm />} />
-       <Route path="/login"element={<LoginForm />}/>
-    </Routes>
+      <Route path="/login"element={<LoginForm />}/>
+
+      <Route path="/post" element={<PrivateRoute><NewPost /></PrivateRoute>} /></Routes>
+    
+    
+    
     </>
   )
 }
---
+++ src/components/ProteccionRuntas.jsx
@@ -0,0 +1,13 @@
+import React, { Children } from 'react'
+import { Navigate } from 'react-router-dom'
+
+const PrivateRoute = ({ children }) => {
+    const token = localStorage.getItem('token')
+
+    if (!token){
+        return <Navigate to='/' replace/>
+    }
+    return children
+}
+
+export default PrivateRoute
